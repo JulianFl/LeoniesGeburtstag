7231cd305f9b62567b4c52d73225a5e574154d16
Julian Fleig <fleig.julian@googlemail.com>
2024-06-30 01:37:55 +0200

    Date

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
     mm = String(today.getMonth() + 1).padStart(2, "0"),
     yyyy = today.getFullYear(),
     nextYear = yyyy + 1,
-    dayMonth = "06/30/",
+    dayMonth = "07/01/",
     birthday = dayMonth + yyyy;
 
   today = mm + "/" + dd + "/" + yyyy;
